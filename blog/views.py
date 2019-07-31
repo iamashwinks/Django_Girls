e1f3588f5a526210cbceb88e01ac7033d3b6ac87
Ashwin <ashwin.k.sri@gmail.com>
2019-07-31 08:38:12 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from blog.models import Post, Comment
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm, CommentForm
+from mixpanel import Mixpanel
+from django.http import JsonResponse
+
+#Token = "b980feec76f2d88d61e1c842d83d06a7 "
+#API Secret = "05a8ce284eb045dcac91b717f39db618"
+mp = Mixpanel("b980feec76f2d88d61e1c842d83d06a7")
+
+
+def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#mp.track(user_id, 'Sent Message')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            mp.track(post.title, 'Created new blog',{'type' : 'text', 'size' : 'large'})
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else: 
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            mp.track(post.title, 'Editted blog')
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit = False)
+			comment.post = post
+			mp.track(comment.text, 'New Comment')
+			comment.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'blog/add_comment_to_post.html', {'form': form})
+	
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
